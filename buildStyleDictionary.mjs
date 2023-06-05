7841879232af0555f4af9b5b5b703eb158123629
shidobecker <juliowebdeveloper@hotmail.com>
2023-06-05 13:23:56 -0300

    First Android Test

--- buildStyleDictionary.mjs
+++ buildStyleDictionary.mjs
@@ -12,6 +12,59 @@ const executeStyleDictionary = (fileName) => {
           destination: 'variables-brand2.scss',
           format: 'scss/variables'
         }]
+      },
+      android: {
+        "transforms": [
+          "attribute/cti",
+          "name/cti/snake",
+          "color/hex8android",
+          "size/sp",
+          "size/dp",
+          "custom/category/lowercase"
+        ],
+        transformGroup: 'android',
+        buildPath: 'android_tokens/src/styles/android/',
+        files: [{
+          destination: 'font_dimens.xml',
+          format: "android/fontDimens"
+        },{
+          destination: "colors.xml",
+          format: "android/colors"
+        }]
+      },
+      compose: {
+        "transforms": [
+          "attribute/cti",
+          "name/cti/snake",
+          "color/hex8android",
+          "size/sp",
+          "size/dp",
+          "custom/category/lowercase"
+        ],
+        transformGroup: 'compose',
+        buildPath: "android_compose_tokens/src/styles/compose/",
+        files: [{
+          destination: "StyleDictionaryColor.kt",
+          format: "compose/object",
+          className: "StyleDictionaryColor",
+          packageName: "StyleDictionaryColor",
+          filter: {
+            attributes: {
+              category: "color"
+            }
+          }
+        },{
+          destination: "StyleDictionarySize.kt",
+          format: "compose/object",
+          className: "StyleDictionarySize",
+          packageName: "StyleDictionarySize",
+          type: "float",
+          filter: {
+            attributes: {
+              category: "size"
+            }
+          }
+        }]
       }
     }
   });
