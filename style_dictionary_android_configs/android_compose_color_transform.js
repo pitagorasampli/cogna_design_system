834831da4799553f70b6e0b4d9064236c75e857b
shidobecker <juliowebdeveloper@hotmail.com>
2023-06-06 18:18:40 -0300

    Transform with cogna colors

--- style_dictionary_android_configs/android_compose_color_transform.js
+++ style_dictionary_android_configs/android_compose_color_transform.js
@@ -2,9 +2,17 @@ const StyleDictionary = require('style-dictionary');
 
 var Color = require('tinycolor2')
 
+const colorNameGenerator = (attributeName, token) => {
+    this.matcher; (token) => token.attribute.category = attributeName,
+    this.transformer; (prop) =>{
+        const hex8 = Color(prop.value).toHex8();
+        return `Color(0x${hex8})`;
+    }
+}
+
 module.exports = {
   type: `value`,
-  matcher: (token) => token.attributes.category === `color`,
+  matcher: (token) => token.attributes.category === `sofia`,
   transformer: (prop) => {
     const hex8 = Color(prop.value).toHex8();
     return `Color(0x${hex8})`;
